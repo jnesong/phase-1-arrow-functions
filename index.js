59fc1425d2da3f416b5a157361dead5c474d2900
Jenny Chau <jnechau@gmail.com>
2021-11-02 22:44:29 -0400

    commit tree 674dca7b25d694a1abd34b304c0eecd9c0e54fb9

---
+++ index.js
@@ -0,0 +1,12 @@
+
+const divide = () => 2000/100
+
+console.log(divide())
+
+const square = par1 => par1*par1
+
+console.log(square(3))
+
+const add = (par2, par3) => par2+par3
+
+console.log(add(4,9))
